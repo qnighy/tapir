e403dd0e3a0eac6891bf1490e738fe5f3eb336ac
Masaki Hara <ackie.h.gmai@gmail.com>
2017-09-15 14:32:57 +0900

    Add -g to CFLAGS.

--- src/common.mk
+++ src/common.mk
@@ -21,7 +21,7 @@ endif
 OBJS = main.o main_rb.o openres.o sdl_misc.o misc.o \
        Bitmap.o Color.o Graphics.o RGSSError.o RGSSReset.o \
        Rect.o Sprite.o Table.o Tone.o
-CFLAGS += -O2 -Wall -Wextra
+CFLAGS += -g -O2 -Wall -Wextra
 ifeq ($(RGSS),3)
 CPPFLAGS += -I$(RUBY_DIR)/include/ruby-1.9.1/$(RUBY_PLATFORM) \
 	    -I$(RUBY_DIR)/include/ruby-1.9.1 \
