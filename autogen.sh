40574d90a181a9ac4217fd29af1bf8e9729fa125
Masaki Hara <ackie.h.gmai@gmail.com>
2017-11-01 15:30:31 +0900

    Adjust distclean.

--- autogen.sh
+++ autogen.sh
@@ -3,5 +3,11 @@ set -ue
 
 aclocal
 autoheader
-automake --force-missing --add-missing
+automake --force-missing --add-missing --copy
 autoconf
+
+if [ -e .git ]; then
+  git submodule update --init
+fi
+cd ruby181; autoconf; cd ..
+cd ruby192; autoconf; cd ..
