b9d9edbd6d9203df9d9498adff13d628b221a483
Masaki Hara <ackie.h.gmai@gmail.com>
2017-11-27 18:34:34 +0900

    Don't pass DATA_DIR which is no more needed.

--- src/common.mk
+++ src/common.mk
@@ -17,8 +17,7 @@ tapir_SOURCES = main.c main_rb.c archive.c openres.c gl_misc.c sdl_misc.c \
 		RGSSError.c RGSSReset.c Rect.c Sprite.c Table.c Tilemap.c \
 		Tone.c Viewport.c Win32APIFake.c Window.c
 
-CFLAGS += $(SDL_CFLAGS) $(GL_CFLAGS) $(FONTCONFIG_CFLAGS) $(LIBCONFIG_CFLAGS) \
-	  -DDATA_DIR='"$(datadir)"'
+CFLAGS += $(SDL_CFLAGS) $(GL_CFLAGS) $(FONTCONFIG_CFLAGS) $(LIBCONFIG_CFLAGS)
 LDFLAGS += -L$(RUBY_DIR)/lib
 LIBS += $(RUBY_SRC_DIR)/ext/zlib/zlib.a -lz \
 	-lruby-static -lpthread -lrt -ldl -lcrypt -lm \
